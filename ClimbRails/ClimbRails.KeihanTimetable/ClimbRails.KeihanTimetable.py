84d4235293d065dbc94a831aabf61d545f96963f
yang peng <854525261@qq.com>
2018-09-30 09:19:32 +0800

    init

---
+++ ClimbRails/ClimbRails.KeihanTimetable/ClimbRails.KeihanTimetable.py
@@ -0,0 +1,44 @@
+#coding=utf-8
+import os
+os.system('cls')
+import urllib.request
+import re
+import sys
+import makeUrl
+
+print('------------------------------')
+
+url=makeUrl.makefromint(40)
+print(url)
+
+try:
+    data=urllib.request.urlopen(url)
+except urllib.error.HTTPError as e:
+    print(e)
+    sys.exit()
+
+datastr=data.read().decode()
+
+l=len(datastr)
+if l>0:
+    print(l)
+#print(datastr)
+
+title_str=r'<title>(.+?)｜電車・駅のご'
+title_comp=re.compile(title_str)
+
+timetable_str=r'<li><span>(.+?)</span><a href="(.+?)" target="_blank"><img src="img/timetable_weekday.gif" alt="(.+?)（PDF）" width="85" height="21" /></a><a href="(.+?)" target="_blank"><img src="img/timetable_holiday.gif" alt="(.+?)（PDF'
+timetable_comp=re.compile(timetable_str)
+
+struct_str=r'<p><a href="(.+?)" target="_blank"><.+? alt="駅構内図'
+struct_comp=re.compile(struct_str)
+
+title=title_comp.findall(datastr)
+timetables=timetable_comp.findall(datastr)
+struct=struct_comp.findall(datastr)
+
+print(title)
+for r in timetables:
+    print(r)
+print(struct)
+
---
+++ ClimbRails/ClimbRails.KeihanTimetable/makeUrl.py
@@ -0,0 +1,10 @@
+def make(str):
+    url = 'https://www.keihan.co.jp/traffic/station/stationinfo/' + str + '.html'
+    return url
+
+def makefromint(n):
+    if n < 10:
+        return 'https://www.keihan.co.jp/traffic/station/stationinfo/' + '00' + str(n) + '.html'
+    if n < 100:
+        return 'https://www.keihan.co.jp/traffic/station/stationinfo/' + '0' + str(n) + '.html'
+    return 'https://www.keihan.co.jp/traffic/station/stationinfo/' + str(n) + '.html'
