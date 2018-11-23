79cf2c329534ae2f70331430cf746577c0bfe45d
yang peng <854525261@qq.com>
2018-11-23 14:24:25 +0800

    bu

--- ClimbRails/RestaurantClimb/pdoutput.py
+++ ClimbRails/RestaurantClimb/pdoutput.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.DataFrame({'key':['a','b','c'],'data1':[1,2,3],'data2':[4,5,6]})  
+ar=[4,5,6]
+df = pd.DataFrame({'key':['a','b','c'],'data1':[1,2,3],'data2':[9,12,ar]})  
 print(df)
 for idx,item in df.iterrows():
     print(idx)
