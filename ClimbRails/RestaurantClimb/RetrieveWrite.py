ea096d369239c41138b1cd69d81907f2813ebd9d
yang peng <854525261@qq.com>
2019-01-12 16:03:03 +0800

    softmax finish

--- ClimbRails/RestaurantClimb/RetrieveWrite.py
+++ ClimbRails/RestaurantClimb/RetrieveWrite.py
@@ -4,13 +4,14 @@ os.system('cls')
 import urllib.request
 import csv
 import re
+import pandas as pd
 
 with open('urls.csv', 'r', newline='') as f:
     rawlist = f.readlines()
 
 restau_raw = []
 for r in rawlist:
-    restau_raw.append(r[:-2])
+    restau_raw.append(r[:-3])
 
 #print(restau_raw)
 review_str = r'"description": "(.+?)", "author": "(.+?)"}'
@@ -23,15 +24,60 @@ location_comp = re.compile(location_str)
 time_comp = re.compile(time_str)
 address_comp = re.compile(address_str)
 
+uniqueidSeries = []
+nameSeries = []
+addressSeries = []
+zipSeries = []
+timeSeries = []
+reviewSeries = []
+
+fp = open('main_info.csv', 'w', newline='')
+fr = open('reviews.csv', 'w', newline='')
+
 for raw in restau_raw:
-    curReview = []
     url = r'https://www.yelp.com/biz/' + raw + r'?osq=Restaurants'
     data = urllib.request.urlopen(url)
     datastr = data.read().decode()
     reviewinfo = review_comp.findall(datastr)
     locationdic = location_comp.findall(datastr)
     timedic = time_comp.findall(datastr)
-    addressinfo=address_comp.findall(datastr)
-    curReview.append(reviewinfo)
+    addressinfo = address_comp.findall(datastr)
 
+    uniqueid = raw
+    uniqueidSeries.append(uniqueid)
+
+    name = addressinfo[0][0]
+    nameSeries.append(name)
+
+    addr = addressinfo[0][3]
+    addressSeries.append(addr)
+
+    zip = addressinfo[0][4]
+    zipSeries.append(zip)
+    
+    timearray = []
+    time = ''
+    for d in timedic:
+        timearray.append([d[0],d[1] + d[2] + d[3]])
+        time = time + '/' + d[0] + ':' + d[1] + d[2] + d[3]
+    timeSeries.append(timearray)
+
+    revarray = []
+    for rev in reviewinfo:
+        for loc in locationdic:
+            if rev[1] == loc[0]:
+                rev_complete = []
+                rev_complete.append(rev[0])
+                rev_complete.append(rev[1])
+                rev_complete.append(loc[1])
+                revarray.append(rev_complete)
+                fr.write(uniqueid + ',' + rev[0] + ',' + rev[1] + ',' + loc[1] + '\n')
+    reviewSeries.append(revarray)
+    
+    fp.write(uniqueid + ',' + name + ',' + addr + ',' + zip + ',' + time + '\n')
+    print("")
+
+df = pd.DataFrame({'key':uniqueidSeries,'name':nameSeries,'address':addressSeries,'zip':zipSeries,'time':timeSeries,'review':reviewSeries})
+fp.close()
+fr.close()
 
--- ClimbRails/TestTensor/learnbinary.py
+++ ClimbRails/TestTensor/learnbinary.py
@@ -10,8 +10,6 @@ def bin2dec(arr):
 
 #lst=[1,0,1,1,0,1,1,0]
 #print(bin2dec(np.asarray(lst)))
-
-#换输入和输出 参数化维数 评估准确度
 inputlist = dataread.input('learn')
 outputlist = dataread.output('learn')
 print()
@@ -23,15 +21,17 @@ x = tf.placeholder(tf.float32, [None, input_dim])
 W = tf.Variable(tf.zeros([input_dim,output_dim]))
 b = tf.Variable(tf.zeros([output_dim]))
 
-y = tf.nn.softmax(tf.matmul(x,W) + b)
-
 y_ = tf.placeholder("float", [None,output_dim])
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 
-train_step = tf.train.GradientDescentOptimizer(0.0002).minimize(cross_entropy)
+# ================================================================
+# main model
+# ================================================================
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.0004).minimize(cross_entropy)
+# ================================================================
 
 init = tf.global_variables_initializer()
-
 sess = tf.Session()
 sess.run(init)
 
@@ -39,18 +39,33 @@ sess.run(init)
 inputtest = dataread.input('validate')
 outputtest = dataread.output('validate')
 
-#print(inputtest)
-#yraw = sess.run(y,feed_dict={x:inputtest})
-#yargmax = sess.run(tf.argmax(y,1),feed_dict={x:inputtest})
-#print(yraw)
-#print(yargmax)
 num = 400001
 for i in range(num):
     sess.run(train_step, feed_dict={x: inputlist, y_: outputlist})
     if i % 10000 == 0:
-        #print(i, sess.run(W), sess.run(b))
         print(i / num)
+        yargmax = sess.run(tf.argmax(y,1),feed_dict={x:inputtest})
+        yargmax_correct = sess.run(tf.argmax(y_,1),feed_dict={y_:outputtest})
+        print(yargmax)
+        print(yargmax_correct)
+        print(sess.run(b))
+        cnt = 0
+        abserror = 0
+        for i in range(50):
+            abserror+=np.abs(yargmax[i] - yargmax_correct[i])
+            if yargmax[i] == yargmax_correct[i]:
+                cnt+=1
+        print(cnt)
+        print(abserror)
+        print()
         #yargmax = sess.run(tf.argmax(y,1),feed_dict={x:inputtest})
-        correct = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
-        accuracy = tf.reduce_mean(tf.cast(correct, "float"))
-        print(accuracy.eval(feed_dict={x: inputtest, y_: outputtest}))
+        #correct = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+        #accuracy = tf.reduce_mean(tf.cast(correct, "float"))
+        #print(accuracy.eval(feed_dict={x: inputtest, y_: outputtest}))
+#print(inputtest)
+#yraw = sess.run(y,feed_dict={x:inputtest})
+#print(yraw)
+
+# best: 37 correct 14 abserror (rate=0.0004)
+# final: 36 correct 17 abserror then nan...nan
+print()
